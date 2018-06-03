370b8feef19cc0b696dab110beb09632aae8bcb2
Ashad Rahman <ashad.cse@gmail.com>
2018-06-03 11:57:16 +0530

    first Commit

---
+++ StringConcept/src/StringA.java
@@ -0,0 +1,16 @@
+
+public class StringA {
+	
+	public static void main(String[] args)
+	{
+		String s1=new String("Durga");
+		s1.concat("software");
+		String s2=s1.concat("Solution");
+		s1=s1.concat("soft");
+		System.out.println(s1);
+		System.out.println(s2);
+		
+	}
+	
+
+}
